19ad5599f526d56b98adb6d27508e9f8c7f26442
Bratee Podder <bpodder3@gatech.edu>
2024-12-04 21:58:59 -0500

    Changes

--- business-supply-frontend/next.config.js
+++ business-supply-frontend/next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path");
+const { loadEnvConfig } = require("@next/env");
 
-module.exports = nextConfig
+const apiEnvPath = path.resolve(__dirname, "../api/");
+loadEnvConfig(apiEnvPath, true);
+
+
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  webpack(config, { isServer }) {
+    // Specify the absolute path to your .env file located in the api/ folder
+    config.plugins.push(
+      new Dotenv({
+        path: path.resolve(__dirname, "../api/.env"), // Adjust the path accordingly
+      }),
+    );
+
+    return config;
+  },
+};
